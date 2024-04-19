0bff28cfedd84177f567a9de8b38d4bab858e8d1
shimaoka hitoshi <shimaoka1203@gmail.com>
2024-04-19 17:29:32 +0900

    デプロイ用router調整(途中)

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,30 @@ import Tasks from './pages/Tasks'
 import Lesson from './pages/Lesson'
 
 function App() {
-	return (
-		// <BrowserRouter>
-		//   <Routes>
-		//     <Route exact path='/' element={<Operate />} />
-		//     <Route exact path='/admin' element={<Admin />} />
-		//   </Routes>
-		// </BrowserRouter>
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Top />}>
-					<Route path='/task/:subjectId' element={<Tasks />} />
-					<Route
-						path='/task/:subjectId/detail/:taskId'
-						element={<Lesson />}
-					/>
-				</Route>
-				<Route path='/admin' element={<Admin />}>
-					{/* elementを追加 */}
-					<Route path='/admin/subject' element={<SubjectMaster />} />
-					<Route path='/admin/task' element={<RegistTask />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	)
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route exact path='/' element={<Operate />} />
+    //     <Route exact path='/admin' element={<Admin />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Top />}>
+          <Route path='/task/:subjectId' element={<Tasks />} />
+          <Route
+            path='/task/:subjectId/detail/:taskId'
+            element={<Lesson />}
+          />
+        </Route>
+        <Route path='/admin' element={<Admin />}>
+          {/* elementを追加 */}
+          <Route path='/admin/subject' element={<SubjectMaster />} />
+          <Route path='/admin/task' element={<RegistTask />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import Test from './Test.tsx'
+import Test2 from './Test2.tsx'
 import './index.css'
+import {
+    createHashRouter,
+    RouterProvider
+} from 'react-router-dom';
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <Test />,
+    },
+    {
+        path: "/test",
+        element: <Test2 />,
+    }
+]);
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+// )
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
